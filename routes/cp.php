cef37a4304d0987beb7a015dcc2bd78d4b01ea93
Stan Menten <84512974+StanMenten-bp@users.noreply.github.com>
2023-06-14 11:51:25 +0200

    Fixed DashboardController not found error

--- routes/cp.php
+++ routes/cp.php
@@ -1,5 +1,7 @@
 <?php
 
+use Statamic\Http\Controllers\CP\DashboardController;
+
 Route::prefix('locale-suggester')->name('statamic-locale-suggester.')->group(function() {
-    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
